31da1f9e25b70f631e852fe24416b5ee6f5008f8
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2024-11-01 10:38:26 -0700

    first packages and tests

--- basic-functionality/main.go
+++ basic-functionality/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote/v3"
+)
+
+func getQuote() string {
+	return quote.HelloV3()
+}
+
+func getNewerQuote() string {
+	return quote.Concurrency()
+}
 
 func main() {
-	fmt.Print("Hi!")
+	fmt.Println("Hi!")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	i := 15
+	if i < 10 {
+		fmt.Println("Less than 10")
+	} else if i < 20 {
+		fmt.Println("Less than 20")
+	}
+
+	fmt.Println(getQuote())
+	fmt.Println(getNewerQuote())
 }
