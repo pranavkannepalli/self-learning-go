3c66f20e7f9e9829ce07cd0f01405d5b8fe86fd6
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2024-11-03 11:14:13 -0800

    feat: base go scraper that visits websites recursively

--- web-scraper/main.go
+++ web-scraper/main.go
@@ -1,6 +1,28 @@
-package webscraper
+package main
 
-import {
+import (
 	"fmt"
-	
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	fmt.Println("Starting webscraper")
+
+	c := colly.NewCollector()
+	c.MaxDepth = 2
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	var website string
+	fmt.Print("Pick a website to scrape: ")
+	fmt.Scanln(&website)
+	c.Visit(website)
 }
